742bfb95eade74bf3129a251fb05e880096df287
Charles Machalow <csm10495@gmail.com>
2023-07-07 16:34:59 -0700

    lint fixes

--- shadowcopy/shadow.py
+++ shadowcopy/shadow.py
@@ -1,26 +1,35 @@
-'''
+"""
 Home to the shadow_copy function
-'''
+"""
 
 import ctypes
 import logging
 import os
-import wmi
-import textwrap
 import shutil
-
+import textwrap
 from pathlib import Path
-from .exceptions import RequiresAdminError, OSUnsupportedError, PathIsNotToFile, ShadowCopyFailure
+
+import wmi
+
+from .exceptions import (
+    OSUnsupportedError,
+    PathIsNotToFile,
+    RequiresAdminError,
+    ShadowCopyFailure,
+)
 
 logger = logging.getLogger(__name__)
 
+
 def _is_os_supported() -> bool:
-    ''' Private method for checking if os is supported '''
-    return os.name == 'nt'
+    """Private method for checking if os is supported"""
+    return os.name == "nt"
+
 
 def _is_admin() -> bool:
-    ''' Private method for checking if we're admin '''
+    """Private method for checking if we're admin"""
     return ctypes.windll.shell32.IsUserAnAdmin() != 0
+
 
 def shadow_copy(src: str | Path, dst: str | Path) -> None:
     """
@@ -30,13 +39,13 @@ def shadow_copy(src: str | Path, dst: str | Path) -> None:
     Shadow copies are normally used to allow in use files to be copied and backed up.
     """
     if not _is_os_supported():
-        raise OSUnsupportedError(f'This OS is not supported: {os.name}')
+        raise OSUnsupportedError(f"This OS is not supported: {os.name}")
 
     if not _is_admin():
-        raise RequiresAdminError('This operation requires admin. Please run as admin.')
+        raise RequiresAdminError("This operation requires admin. Please run as admin.")
 
     if not Path(src).is_file():
-        raise PathIsNotToFile(f'The src path is not a file: {src}')
+        raise PathIsNotToFile(f"The src path is not a file: {src}")
 
     # Need fully resolved path for shadow copy
     src = Path(src).resolve()
@@ -46,17 +55,23 @@ def shadow_copy(src: str | Path, dst: str | Path) -> None:
     drive_letter_with_colon, rest_of_path = os.path.splitdrive(src)
     rest_of_path = rest_of_path.lstrip(os.sep)
 
-    result, shadow_id = c.Win32_ShadowCopy.Create(Context='ClientAccessible', Volume=f'{drive_letter_with_colon}{os.sep}')
+    result, shadow_id = c.Win32_ShadowCopy.Create(
+        Context="ClientAccessible", Volume=f"{drive_letter_with_colon}{os.sep}"
+    )
 
     if result != 0:
-        raise ShadowCopyFailure(textwrap.dedent(f'''
+        raise ShadowCopyFailure(
+            textwrap.dedent(
+                f"""
                 Unable to create a shadow copy of the source file.
-                Win32_ShadowCopy. Create Error: {result}.'''))
+                Win32_ShadowCopy. Create Error: {result}."""
+            )
+        )
 
     shadow_obj = c.Win32_ShadowCopy(ID=shadow_id)[0]
     try:
         shadow_volume_path = shadow_obj.DeviceObject
-        logger.debug(f'shadow volume path: {shadow_volume_path}')
+        logger.debug(f"shadow volume path: {shadow_volume_path}")
         # note that os.path.join(..) does not work properly with shadow_volume_path. It would start with \\?\GLOBALROOT...
         shutil.copy(shadow_volume_path + os.sep + rest_of_path, dst)
     finally:
--- tests/test_shadowcopy.py
+++ tests/test_shadowcopy.py
@@ -1,54 +1,60 @@
-'''
+"""
 Home to tests for shadowcopy
-'''
+"""
 
-import pytest
 from unittest.mock import patch
 
+import pytest
+
+from shadowcopy.exceptions import (
+    OSUnsupportedError,
+    PathIsNotToFile,
+    RequiresAdminError,
+)
 from shadowcopy.shadow import _is_admin, shadow_copy
-from shadowcopy.exceptions import OSUnsupportedError, RequiresAdminError, PathIsNotToFile
 
 
-@pytest.fixture(scope='function')
+@pytest.fixture(scope="function")
 def admin_required():
     if not _is_admin():
         pytest.skip("Skipping because this test requires admin")
 
 
 def test_shadow_copy_full(tmp_path, admin_required):
-    src = tmp_path / 'src.txt'
-    src.write_text('hello world')
+    src = tmp_path / "src.txt"
+    src.write_text("hello world")
 
-    dst = tmp_path / 'dst.txt'
-    dst.write_text('goodbye world')
+    dst = tmp_path / "dst.txt"
+    dst.write_text("goodbye world")
 
     assert shadow_copy(src, dst) is None
 
-    assert dst.read_text() == 'hello world'
-    assert src.read_text() == 'hello world'
+    assert dst.read_text() == "hello world"
+    assert src.read_text() == "hello world"
+
 
 def test_shadow_copy_raises_osunsupported():
-    with patch('shadowcopy.shadow._is_os_supported', return_value=False):
-        with patch('shadowcopy.shadow.os.name', 'lol'):
+    with patch("shadowcopy.shadow._is_os_supported", return_value=False):
+        with patch("shadowcopy.shadow.os.name", "lol"):
             with pytest.raises(OSUnsupportedError) as ex:
-                shadow_copy('src.txt', 'dst.txt')
+                shadow_copy("src.txt", "dst.txt")
 
-    assert str(ex.value) == 'This OS is not supported: lol'
+    assert str(ex.value) == "This OS is not supported: lol"
 
 
 def test_shadow_copy_raises_requiresadmin():
-    with patch('shadowcopy.shadow._is_os_supported', return_value=True):
-        with patch('shadowcopy.shadow._is_admin', return_value=False):
+    with patch("shadowcopy.shadow._is_os_supported", return_value=True):
+        with patch("shadowcopy.shadow._is_admin", return_value=False):
             with pytest.raises(RequiresAdminError) as ex:
-                shadow_copy('src.txt', 'dst.txt')
+                shadow_copy("src.txt", "dst.txt")
 
-        assert str(ex.value) == 'This operation requires admin. Please run as admin.'
+        assert str(ex.value) == "This operation requires admin. Please run as admin."
 
 
 def test_shadow_copy_raises_pathisnottofile(tmp_path):
-    with patch('shadowcopy.shadow._is_os_supported', return_value=True):
-        with patch('shadowcopy.shadow._is_admin', return_value=True):
+    with patch("shadowcopy.shadow._is_os_supported", return_value=True):
+        with patch("shadowcopy.shadow._is_admin", return_value=True):
             with pytest.raises(PathIsNotToFile) as ex:
-                shadow_copy(tmp_path, 'dst.txt')
+                shadow_copy(tmp_path, "dst.txt")
 
-    assert str(ex.value) == f'The src path is not a file: {tmp_path}'
+    assert str(ex.value) == f"The src path is not a file: {tmp_path}"
